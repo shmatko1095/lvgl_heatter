ca0ec678655e3d5761d9bab54a1f5f58c80d0110
Oleksandr Shmatko <oleksandr.shmatko@globallogic.com>
2021-10-25 22:41:38 +0300

    OSH Frimvare project restart
    Signed-off-by: Oleksandr Shmatko <oleksandr.shmatko@globallogic.com>

--- main/component.mk
+++ main/component.mk
@@ -2,7 +2,3 @@
 # "main" pseudo-component makefile.
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-
-CFLAGS+= -DLV_LVGL_H_INCLUDE_SIMPLE
-CXXFLAGS+= -DLV_LVGL_H_INCLUDE_SIMPLE 
-COMPONENT_EMBED_TXTFILES := ConnectionApp/certs/coap_ca.pem ConnectionApp/certs/coap_client.crt ConnectionApp/certs/coap_client.key
